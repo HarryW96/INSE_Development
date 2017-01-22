4ba6ab135cf88e61e2237042ca56447950d5f075
Shaun Porter <shaunoman840@gmail.com>
2017-01-22 18:31:16 +0000

    Implemented Registration!
    Registration is now functional! Validation is required, but it works!
    :fireworks:

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
+// Express and AJAX
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+// Database and MySQL
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "bookit",
+  password: "1234",
+  database: "test"
+})
+//Initalization
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(express.static(__dirname + "/"));
+//Code Body
 
 app.post("/user/register", function(req,res){
-  console.log("Got somthing!");
-  console.log(req.body.name);
+  console.log(req.body);
+  addUserToDatabase(req.body);
+
 
   res.send("Got it!");
 })
+
+function addUserToDatabase(user){
+  connection.connect();
+
+  connection.query("INSERT INTO user SET ?", {Name: user.name, Email: user.email, Password: user.pass}, function(err,result){
+    if (err) throw err;
+
+  });
+
+  connection.end();
+}
 
 app.listen(8080);
