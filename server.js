33295d0db6bf57f3445992d81dec3ba91cc191a5
Shaun Porter <shaunoman840@gmail.com>
2017-01-23 15:21:23 +0000

    Registration page no longer crashes after registering twice.

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ var app = express();
 var bodyParser = require("body-parser");
 // Database and MySQL
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "bookit",
-  password: "1234",
-  database: "test"
-})
 //Initalization
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -25,6 +19,12 @@ app.post("/user/register", function(req,res){
 })
 
 function addUserToDatabase(user){
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "bookit",
+    password: "1234",
+    database: "test"
+  })
   connection.connect();
 
   connection.query("INSERT INTO user SET ?", {Name: user.name, Email: user.email, Password: user.pass}, function(err,result){
