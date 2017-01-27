537f76bcfe77bb24201c8ba08a9b5a6368b34cd9
Shaun Porter <shaunoman840@gmail.com>
2017-01-27 18:53:10 +0000

    Moved file structure around to fit express-session
    Hope to implement login sessions tonight.

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 // Express and AJAX
 var express = require("express");
+var session = require("express-session");
 var app = express();
 var bodyParser = require("body-parser");
 // Database and MySQL
 var mysql = require("mysql");
 //Initalization
+app.use(express.static(__dirname + "/pages"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/"));
+app.use(session({secret: "ssshhhhh"}));
 //Code Body
+var sess
+
+app.get("/",function(req,res){
+  console.log("heyo");
+  res.sendFile(__dirname + "/pages/home.html");
+})
 
 //Register a new user
 app.post("/user/register", function(req,res){
   addUserToDatabase(req.body);
   res.send(req.body.name + " was registered sucessfully.");
   console.log(req.body.name + " was registered sucessfully.")
+  res.end("done")
 })
-
 
 // Create a new user entry in the database using a JSON file conisting of thier submited details.
 function addUserToDatabase(user){
