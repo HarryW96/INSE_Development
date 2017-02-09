cb5e8f288b1d815e88efd83162e3a8251bab120a
Harry Westbrook <ig3nharry@gmail.com>
2017-02-09 12:17:57 +0000

    Began work on event.js
    Began working on javascript for event page.

---
+++ pages/assets/js/event.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+
+function getEventDetails(){
+  var eventHeading = document.getElementById("event_heading");
+  var eventImage = document.getElementById("event_image");
+  var eventDesc = document.getElementById("event_description");
+  var eventLoc = document.getElementById("event_location_wrap");
+
+}
