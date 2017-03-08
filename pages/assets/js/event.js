4a111cf02e129d0c857497e7b351ea5b70b19d14
mkasase <mkasase@gmail.com>
2017-03-08 15:15:29 +0000

    Update event.js

--- pages/assets/js/event.js
+++ pages/assets/js/event.js
@@ -1,9 +1,13 @@
 var xhr = new XMLHttpRequest();
 
 function getEventDetails(){
+  // adds the event heading 
   var eventHeading = document.getElementById("event_heading");
+  // adds the event image 
   var eventImage = document.getElementById("event_image");
+  // adds  the event Description
   var eventDesc = document.getElementById("event_description");
+  // adds the event location 
   var eventLoc = document.getElementById("event_location_wrap");
 
 }
