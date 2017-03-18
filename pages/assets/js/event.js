18b7021a2d38bfff8fe7e1985e0ad25e8830f41f
Harry Westbrook <ig3nharry@gmail.com>
2017-03-18 09:56:12 +0000

    xhr request on /event for event data.
    Nearly complete just needs JSON data to be applied to .innertext of
    elements.
    
    May need to break up HTML using spans to be able to properly apply some
    data such as date and times due to formatting.

--- pages/assets/js/event.js
+++ pages/assets/js/event.js
@@ -1,17 +1,28 @@
 var xhr = new XMLHttpRequest();
 
-<<<<<<< HEAD
-function getEvent(){
-=======
-function getEventDetails(){
-  // adds the event heading 
->>>>>>> origin/master
-  var eventHeading = document.getElementById("event_heading");
-  // adds the event image 
-  var eventImage = document.getElementById("event_image");
-  // adds  the event Description
-  var eventDesc = document.getElementById("event_description");
-  // adds the event location 
-  var eventLoc = document.getElementById("event_location_wrap");
+function getEventPage(){
+  var titleEle = document.getElementById("event_heading");
+  var imageEle = document.getElementById("event_image");
+  var descrpEle = document.getElementById("event_description");
 
+  xhr.open("GET", "/event");
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == XMLHttpRequest.DONE){ //TODO replace all (xhr.readystate && xhr.statusCode) with this new format
+      console.log("Ready!");
+      if(xhr.status == 200){
+        console.log("Ready to update event page!");
+        var profileData = JSON.parse(xhr.responseText);
+
+        //may need to use spans in html to place data in right place for date's and times
+        titleEle.innerText = //JSON Data
+        descrpEle.innerText = //JSON Data
+      }
+      else if(xhr.status == 401){
+        window.location = "./event.html";
+        titleEle.innerText = "No event here!";
+        descrpEle.innerText = "";
+      }
+    }
+  }
+  xhr.send(null);
 }
