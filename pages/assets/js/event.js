82591bf2faed6752626e049aaa58a5a88ca53cd8
Shaun Porter <shaunoman840@gmail.com>
2017-03-18 12:33:19 +0000

    Event pages now load dynamically depending on their URL value.

--- pages/assets/js/event.js
+++ pages/assets/js/event.js
@@ -4,25 +4,27 @@ function getEventPage(){
   var titleEle = document.getElementById("event_heading");
   var imageEle = document.getElementById("event_image");
   var descrpEle = document.getElementById("event_description");
+  var capacityEle = document.getElementById("event_capacity");
+  var eventID = window.location.search.substring(1);
 
-  xhr.open("GET", "/event");
+  xhr.open("GET", "/event?eventID=" + eventID);
   xhr.onreadystatechange = function(){
-    if(xhr.readyState == XMLHttpRequest.DONE){ //TODO replace all (xhr.readystate && xhr.statusCode) with this new format
-      console.log("Ready!");
-      if(xhr.status == 200){
-        console.log("Ready to update event page!");
-        var profileData = JSON.parse(xhr.responseText);
-
-        //may need to use spans in html to place data in right place for date's and times
-        titleEle.innerText = //JSON Data
-        descrpEle.innerText = //JSON Data
+    if(xhr.readyState == XMLHttpRequest.DONE){
+      if(xhr.status == 200){ // Populate elements if valid
+        var profileData = JSON.parse(xhr.responseText)[0]
+        titleEle.innerText = profileData.event_Name;
+        descrpEle.innerText = profileData.descrp;
+        capacityEle.innerText = profileData.capacity;
       }
       else if(xhr.status == 401){
-        window.location = "./event.html";
         titleEle.innerText = "No event here!";
         descrpEle.innerText = "";
+        capacityEle.innerText = "";
       }
     }
   }
   xhr.send(null);
 }
+
+
+window.addEventListener("load", getEventPage)
