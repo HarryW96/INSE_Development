67d067cc2bb94f2627e6f6e57bcf27003510418b
Shaun Porter <shaunoman840@gmail.com>
2017-04-15 22:25:13 +0100

    Ticket bindings now implemented.

--- pages/assets/js/ticket.js
+++ pages/assets/js/ticket.js
@@ -1,5 +1,34 @@
 function buildTicket(){
-  
+  xhr = new XMLHttpRequest();
+
+  var id = getQueryVariable("id");
+  var eventName = document.getElementById("event-name");
+  var eventDate = document.getElementById("event-date");
+  //var eventPerson = document.getElementById("TOBEDONE")
+  var eventImage = document.getElementById("event-img");
+  xhr.open("GET","/ticket?id=" + id);
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+      var ticket = JSON.parse(xhr.responseText)[0];
+      eventName.innerText = ticket.event_name;
+      eventDate.innerText = ticket.event_date;
+      eventImage.setAttribute("src","../ticket/img?q=" + ticket.event_img);
+      console.log(ticket)
+    }
+  }
+  xhr.send();
+}
+
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
 
 window.onload = function(){
