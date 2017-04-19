75720a764992f25e3f08efa2d2b9157d11d70d4e
Shaun Porter <shaunoman840@gmail.com>
2017-04-19 22:41:32 +0100

    Update Database.sql

--- temp/Database.sql
+++ temp/Database.sql
@@ -1,38 +1,3 @@
-create database if not exists bookit;
-
-/*enables the system to store information to user, event and ticket*/
-CREATE TABLE IF NOT EXISTS bookit.user(
-  U_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  fName VARCHAR(20) NOT NULL,
-  lName VARCHAR(20) NOT NULL,
-  dob DATE NOT NULL,
-  address VARCHAR(45),
-  email VARCHAR(45),
-  phoneNum VARCHAR(30),
-  password VARCHAR(20)
-);
-
-CREATE TABLE IF NOT EXISTS bookit.event(
-  E_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  event_Name VARCHAR(40) NOT NULL,
-  eDate DATE NOT NULL,
-  location VARCHAR(40) NOT NULL,
-  capacity INT NOT NULL,
-  descrp VARCHAR(300) NOT NULL,
-  image VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS bookit.ticket (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `event_name` varchar(45) NOT NULL,
- `event_date` date NOT NULL,
- `user_name` varchar(45) NOT NULL,
- `user_id` int(11) NOT NULL,
- `event_img` varchar(100) DEFAULT NULL,
- PRIMARY KEY (`id`)
-);
-
-/* OLD TABLE STRUCTURE ABOVE */
 
 create database if not exists bookit;
 
