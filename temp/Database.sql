25c9869c8f5f8d01e14e1f33bb6aee2c9744ac5d
Techbox26 <technowhiz1@gmail.com>
2017-04-19 23:29:54 +0100

    Changes
    Added new users and events with approptiate images

--- temp/Database.sql
+++ temp/Database.sql
@@ -34,3 +34,11 @@ CREATE TABLE `user` (
  `profile_ref` varchar(100) DEFAULT NULL,
  PRIMARY KEY (`U_ID`)
 );
+
+INSERT INTO `user` (`U_ID`, `fName`, `lName`, `dob`, `address`, `email`, `phoneNum`, `password`, `profile_ref`) VALUES ('1', 'James', 'Test', '1996/10/26', '1 Street', 'test@test.com', '123456789', 'test', NULL);
+
+INSERT INTO `user` (`U_ID`, `fName`, `lName`, `dob`, `address`, `email`, `phoneNum`, `password`, `profile_ref`) VALUES ('2', 'Alfred', 'Peterson', '1965/02/02', '1 Street', 'alf@pet.com', '123456789', 'alf', NULL);
+
+INSERT INTO `event` (`E_ID`, `event_Name`, `eDate`, `location`, `capacity`, `descrp`, `image`) VALUES ('1', 'Deadmau5 Live', '2017/11/22', 'Portsmouth Guildhall', '200', 'Deadmau5 takes the stage for a night of the SESH!', 'deadmau5.jpg');
+INSERT INTO `event` (`E_ID`, `event_Name`, `eDate`, `location`, `capacity`, `descrp`, `image`) VALUES ('2', 'Shaun\'s Savage Sesh', '2017/04/29', 'Portsmouth University Libary', '1000', 'Shaun sets it off with a large mix of educational hits!', 'shaunevent.png');
+INSERT INTO `event` (`E_ID`, `event_Name`, `eDate`, `location`, `capacity`, `descrp`, `image`) VALUES ('3', 'Harry\'s Wild Ride', '2017/12/12', 'BK1.01', '50', 'Light vibes before Christmas', 'harryevent.jpg');
